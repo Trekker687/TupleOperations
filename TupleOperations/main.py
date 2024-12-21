188637cf9a92646f2a0f5c94f4e74a950b4167b4
Trekker687 <113757673+Trekker687@users.noreply.github.com>
2024-12-21 17:16:38 +0530

    Add files via upload

---
+++ TupleOperations/main.py
@@ -0,0 +1,18 @@
+tuplex = ("tuple", False, 3.2, 1)
+print(tuplex)
+
+tupley = (4,6,8,2,1)
+print(tupley)
+
+tuplez = tupley + tuplex
+print(tuplez)
+
+tuple1 = (50,10,60,70,20)
+print(tuple1.count(50))
+
+tuplea = (2,3,4,5,9,5,8)
+slice = tuplex[3:5]
+print(slice)
+
+slice1 = tuplea[6]
+print(slice1)
